39ec6ca579cf3b236d7629cd012b1efca9135ccd
Clément Barberis <barberisclementpro@gmail.com>
2025-07-31 21:11:15 +0200

    feat(ui): add sonner ui component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { Toaster } from 'sonner';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster position="top-center" />
   </StrictMode>
 );
