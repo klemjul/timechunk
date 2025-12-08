4bdd30e03a1368daf5105a90d443df945ae0a22e
Clément Barberis <barberisclementpro@gmail.com>
2025-12-08 21:29:35 +0100

    chore: fix esling.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,25 @@
 import prettier from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import json from '@eslint/json';
 import { globalIgnores } from 'eslint/config';
 
-export default tseslint.config([
+export default defineConfig(
   globalIgnores(['node_modules', 'dist']),
   prettier,
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      'react-hooks': reactHooks,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
@@ -33,5 +37,5 @@ export default tseslint.config([
       'json/no-duplicate-keys': 'error',
       'no-irregular-whitespace': 'off',
     },
-  },
-]);
+  }
+);
